b326aa6d91de50d489f4733c19c7cfce4120e00b
Claudia Ifrim <ifrim.claudia@gmail.com>
2018-05-10 16:21:06 +0300

    initial commit - Prevention and Emergency Protocol

---
+++ pep/src/assets/form.js
@@ -0,0 +1,118 @@
+var form = {
+  tab_1: {
+    label: 'Part I: LEGAL AND REGULATORY MEASURES',
+    data: {
+      question: 'Has the Party, in accordance with Article 14 of the Barcelona Convention, adopted legislation implementing the provisions of the Prevention and Emergency Protocol, as listed in Table I below?',
+      table_label: 'Table I – LEGAL AND REGULATORY MEASURES',
+      articles: [{
+          article_title: 'Article 4.1',
+          article_items: [{
+            description: 'TODO',
+            items: [{
+                type: 'changes',
+                label: 'Changes in the information provided in the previous report',
+                info: 'Please tick the box that applies',
+                selected: null,
+                options: [
+                  { text: 'Yes', value: true },
+                  { text: 'No', value: false }
+                ],
+                options_description: ['If your answer is “Yes”, please update accordingly', 'If your answer is “No”, please go to next question']
+              },
+              {
+                type: 'status',
+                label: 'Status',
+                info: 'Please tick the box that applies',
+                selected: null,
+                options: [
+                  { text: 'Yes', value: 1 },
+                  { text: 'No', value: 2 },
+                  { text: 'Under development', value: 3 },
+                  { text: 'Not applicable', value: 4 }
+                ],
+                options_description: ['If your answer is “Yes”, on a voluntary basis please provide title, date of enactment and a summary of the relevant provisions (Indicate website/URL link or other reference)', 'If your answer is “No”, please in the column difficulties/challenges, tick all that apply'],
+                comments: ''
+              },
+              {
+                type: 'difficulties',
+                label: 'Difficulties/Challenges',
+                info: 'Please tick all that apply',
+                selected: [],
+                options: [
+                  { text: 'Policy framework', value: 1 },
+                  { text: 'Regulatory framework', value: 2 },
+                  { text: 'Financial resources', value: 3 },
+                  { text: 'Administrative management', value: 4 },
+                  { text: 'Technical Guidance Capabilities', value: 5 }
+                ],
+                options_description: ['On a voluntary basis, please briefly describe difficulties/challenges and the type of attention or assistance that is required'],
+                comments: ''
+              }
+            ]
+          }]
+        }
+      ]
+    }
+  },
+  tab_2: {
+    label: 'Part II: POLLUTION PREPAREDNESS AND RESPONSE: OPERATIONAL MEASURES',
+    data: {
+      question: '',
+      table_label: 'Table II – OPERATIONAL MEASURES',
+      articles: [{
+          article_title: 'TODO',
+          article_items: [{
+            description: 'TODO',
+            items: [{
+                type: 'changes',
+                label: 'Changes in the information provided in the previous report',
+                info: 'Please tick the box that applies',
+                selected: null,
+                options: [
+                  { text: 'Yes', value: true },
+                  { text: 'No', value: false }
+                ],
+                options_description: ['If your answer is “Yes”, please update accordingly', 'If your answer is “No”, please go to next question']
+              },
+              {
+                type: 'status',
+                label: 'Status',
+                info: 'Please tick the box that applies',
+                selected: null,
+                options: [
+                  { text: 'Yes', value: 1 },
+                  { text: 'No', value: 2 },
+                  { text: 'Under development', value: 3 },
+                  { text: 'Not applicable', value: 4 }
+                ],
+                options_description: ['If your answer is “Yes”, on a voluntary basis please provide title, date of enactment and a summary of the relevant provisions (Indicate website/URL link or other reference)', 'If your answer is “No”, please in the column difficulties/challenges, tick all that apply'],
+                comments: ''
+              },
+              {
+                type: 'difficulties',
+                label: 'Difficulties/Challenges',
+                info: 'Please tick all that apply',
+                selected: [],
+                options: [
+                  { text: 'Policy framework', value: 1 },
+                  { text: 'Regulatory framework', value: 2 },
+                  { text: 'Financial resources', value: 3 },
+                  { text: 'Administrative management', value: 4 },
+                  { text: 'Technical Guidance Capabilities', value: 5 }
+                ],
+                options_description: ['On a voluntary basis, please briefly describe difficulties/challenges and the type of attention or assistance that is required'],
+                comments: ''
+              }
+            ]
+          }]
+        }
+      ]
+    }
+  },
+  tab_3: {
+    label: 'Part III: POLLUTION INCIDENTS',
+    data: {}
+  }
+}
+
+export default form
