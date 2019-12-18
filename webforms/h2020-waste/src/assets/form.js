239a04f7a88886f2a9688320f95cf4340febf285
Claudia Ifrim <ifrim.claudia@gmail.com>
2019-12-18 18:16:16 +0200

    init webforms - H2020

---
+++ webforms/h2020-waste/src/assets/form.js
@@ -0,0 +1,193 @@
+var form = {
+  country: {
+    tables: {
+      table_1: [{
+          name: 'partyname',
+          type: 'text',
+          label: 'Contracting Party',
+          selected: '',
+        },
+        {
+          name: 'rep_period_from',
+          type: 'number',
+          label: 'Reporting period from (year)',
+          selected: '',
+        },
+        {
+          name: 'rep_period_to',
+          type: 'number',
+          label: 'Reporting period to (year)',
+          selected: '',
+        },
+        {
+          name: 'institution_name',
+          type: 'text',
+          label: 'Name of the institution/s responsible for the Hazardous Wastes Protocol',
+          selected: '',
+        },
+        {
+          name: 'officer_name',
+          type: 'text',
+          label: 'Name of the officer who is the focal point for the Hazardous Wastes Protocol',
+          selected: '',
+        },
+        {
+          name: 'mailing_address',
+          type: 'text',
+          label: 'Mailing address',
+          selected: '',
+        },
+        {
+          name: 'tel',
+          type: 'text',
+          label: 'Tel.',
+          selected: '',
+        },
+        {
+          name: 'fax',
+          type: 'text',
+          label: 'Fax',
+          selected: '',
+        },
+        {
+          name: 'email',
+          type: 'email',
+          label: 'Email',
+          selected: '',
+        }
+      ],
+
+      table_2: [{
+          name: 'contact_point',
+          type: 'text',
+          label: 'Contact point for the national report, if any',
+          selected: '',
+        },
+        {
+          name: 'instituion_full_name',
+          type: 'text',
+          label: 'Full name of the institution',
+          selected: '',
+        },
+        {
+          name: 'national_mailing_address',
+          type: 'text',
+          label: 'Mailing address',
+          selected: '',
+        },
+        {
+          name: 'national_tel',
+          type: 'text',
+          label: 'Tel.',
+          selected: '',
+        },
+        {
+          name: 'national_fax',
+          type: 'text',
+          label: 'Fax',
+          selected: '',
+        },
+        {
+          name: 'national_email',
+          type: 'email',
+          label: 'Email',
+          selected: '',
+        },
+        {
+          name: 'national_signature',
+          type: 'text',
+          label: 'Signature of the Protocol’s Focal Point',
+          selected: '',
+        },
+        {
+          name: 'national_date',
+          type: 'date',
+          label: 'Date of submission of the Report',
+          selected: '',
+        }
+      ],
+
+      table_3: [{
+          name: 'org_name',
+          type: 'text',
+          label: 'Full name of the institution',
+          selected: '',
+        },
+        {
+          name: 'org_contact_point',
+          type: 'text',
+          label: 'Name of the contact point (optional)',
+          selected: '',
+        },
+
+        {
+          name: 'org_tel',
+          type: 'text',
+          label: 'Tel.',
+          selected: '',
+        },
+        {
+          name: 'org_fax',
+          type: 'text',
+          label: 'Fax',
+          selected: '',
+        },
+        {
+          name: 'org_email',
+          type: 'email',
+          label: 'Email',
+          selected: '',
+        }
+      ]
+    }
+
+  },
+  tab_1: {
+    label: 'Part I: Dummy title',
+    collection_id: 713,
+    parent_collection_id: 7,
+    data: {
+      question: '',
+      table_label: 'Table I – Dummy table',
+      collection_id: 714,
+      parent_collection_id: 713,
+        articles: [{
+          article_title: 'Requirements of Articles 5.5 and 9: Prevention and punishment of illegal traffic of hazardous wastes',
+          article_items: [{
+            label: 'Number of inspections',
+            type: 'number',
+            name: 'inspections_number',
+            selected: '',
+          }, {
+            label: 'Number of contraventions',
+            type: 'number',
+            name: 'contraventions_number',
+            selected: '',
+          }, {
+            label: 'Number of criminal sanctions applied',
+            type: 'number',
+            name: 'sanctions_number',
+            selected: '',
+          }, {
+            label: 'Number of other enforcement measures applied',
+            type: 'number',
+            name: 'enf_measures_number',
+            selected: '',
+          }, {
+            label: 'Number of clean measures implemented',
+            type: 'text',
+            name: 'clean_measures_number',
+            selected: '',
+          },
+          {
+            label: 'Remarks/Comments',
+            type: 'textarea',
+            name: 'remarks',
+            selected: '',
+          }]
+        }]
+    }
+  }
+}
+
+export default form
