7e4450d515d642048c5276b2ccb538cef8ff04b1
Claudia Ifrim <ifrim.claudia@gmail.com>
2020-01-07 02:12:21 +0200

    cleanup

--- webforms/h2020-demographic/src/main.js
+++ webforms/h2020-demographic/src/main.js
@@ -1,13 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 import store from './store'
-// import 'date-input-polyfill'
 
 import './polyfills.js'
 
 import BootstrapVue from 'bootstrap-vue'
 import './css/main.scss'
-// import Promise from './polyfills.js'
 
 Vue.use(BootstrapVue);
 // Vue.config.productionTip = false
